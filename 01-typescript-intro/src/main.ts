03a04b568827093c002536a3c480934abb605a61
Wilmer Guedez <w.guedex@gmail.com>
2024-02-16 10:20:16 +0100

    Bases de Typescript: tarea sobre objetos e interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -4,6 +4,7 @@ import { setupCounter } from './counter.ts'
 import './topics/01-basic-types.ts'
 import './topics/02-object-interface.ts'
 import './topics/03-functions.ts'
+import './topics/04-homework-types.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola Mundo`
 console.log('Hola Mundo')
