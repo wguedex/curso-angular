d1aaf17bb3f98ddd551c386f0ad1ce36f44d46c1
Wilmer Guedez <w.guedex@gmail.com>
2024-02-16 13:11:43 +0100

    Bases de Typescript: Desestructuracion de argumentos

--- 01-typescript-intro/src/topics/05-basic-destructuring.ts
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -28,4 +28,15 @@ console.log(song)
 const {song:anotherSong, songDuration:duration} = audioPlayer
 console.log(duration)
 
+
+//Desestructuracion de arreglos: 
+const dbz: string[] = ['Goku','Vegetta','Trunks']
+const [p1, p2, p3]: string[] = ['Goku','Vegetta','Trunks']
+const trunks = dbz[2] || 'Personaje no encontrado' 
+
+console.error('Personaje 3:',trunks)
+console.error('Otro p2:',p2)
+
+
+
 export {}
