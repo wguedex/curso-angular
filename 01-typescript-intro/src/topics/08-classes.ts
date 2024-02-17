38584858abee6a12a1538bf416a2c20f4dcf1146
Wilmer Guedez <w.guedex@gmail.com>
2024-02-17 14:23:33 +0100

    Bases de Typescript: Classes - manejo de constructor

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -2,13 +2,18 @@
 
 export class Person {
     
-    public name?: string;
-    private address?: string;
+    // public name?: string;
+    // private address?: string;
 
-    constructor(name?:string, address?:string) {
-        this.name = name
-        this.address=address
-    }
+    // constructor(name?:string, address?:string) {
+    //     this.name = name
+    //     this.address=address
+    // } 
+
+    constructor(
+        public name?:string, 
+        private address?:string
+        ) {}
 }
 
 const iroman = new Person('Wilmer', 'Espana')
