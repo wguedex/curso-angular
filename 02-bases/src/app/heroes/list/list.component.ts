9acb2a64e91f36b123a072768888643f9ccc0ca5
Wilmer Guedez <w.guedex@gmail.com>
2024-02-23 12:09:46 +0100

    Bases de Angular: ng-template y ngif-else

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class HeroesListComponent {
+
   public heroNames : string[] = ['Spiderman', 'Ironman', 'Hulk', 'She Hulk', 'Thor'];
+  public deleteHero?: string;
+
+  removeLastHero():void {
+    //Borrar el ultimo heroe de la lista
+    this.deleteHero = this.heroNames.pop();
+    console.log(this.deleteHero)
+  }
+
 }
