15ad87e5fb86cbcad932ffe6267cc4387562cb5b
Wilmer Guedez <w.guedex@gmail.com>
2024-02-25 22:45:53 +0100

    Expandir Bases de Angular: agregar AddCharacter component

---
+++ 02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-addcharacter',
+  templateUrl: './add-character.component.html',
+  styleUrl: './add-character.component.css'
+})
+export class AddCharacterComponent {
+
+}
