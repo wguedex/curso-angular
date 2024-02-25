e642e8c8c8a67479c711bb86143e3ceab52c4d37
Wilmer Guedez <w.guedex@gmail.com>
2024-02-25 22:28:36 +0100

    Expandir Bases de Angular: agregar lista de dbz

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent
   ],
   imports: [
     CommonModule
