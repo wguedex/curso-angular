2a91515588031b1e46b43cc5486475b3ced54d0c
Wilmer Guedez <w.guedex@gmail.com>
2024-02-29 23:32:00 +0100

    GifsApp project: diseño y estructura inicial del proyecto

--- 03-gifs-app/src/app/app.module.ts
+++ 03-gifs-app/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    GifsModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
