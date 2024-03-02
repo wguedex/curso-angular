f4b47e4726495b09edd2879e96a9bb9f165a9c9e
Wilmer Guedez <w.guedex@gmail.com>
2024-03-02 13:24:08 +0100

    GifsApp project: validaciones al servicio

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -12,8 +12,27 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
+  private _organizeHistory(tag:string) {
+    tag = tag.toLowerCase();
+
+    //Validar si existe el tag, si existe no incluir en el history
+    if (this._tagHistory.includes(tag)){
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !==tag )
+    }
+
+  //incluir tag en el history
+  this._tagHistory.unshift(tag)
+  this._tagHistory = this.tagHistory.splice(0,10)
+
+  }
+
   public searchTag(tag: string): void {
-    console.log('Desde el servicio')
-    this._tagHistory.unshift(tag);
+
+    if (tag.length === 0 ) return;
+
+    this._organizeHistory(tag)
+
+    // console.log('Desde el servicio')
+    // this._tagHistory.unshift(tag);
   }
 }
