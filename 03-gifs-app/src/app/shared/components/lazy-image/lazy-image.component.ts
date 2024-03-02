400e3c4bb1b1e2753518ee06ecb8ccb482c81f8d
Wilmer Guedez <w.guedex@gmail.com>
2024-03-02 23:02:39 +0100

    GifsApp project: crear componente lazyimage parte 2

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, input } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrl: './lazy-image.component.css'
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url: string = '';
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded : boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url)  {
+      throw new Error('URL property is required');
+    }
+  }
+
+  onLoad() {
+    console.log('Image loaded')
+    this.hasLoaded = true;
+  }
+
 
 }
