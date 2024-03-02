127ba92d2b2ec5625ad339cce6709a657be44119
Wilmer Guedez <w.guedex@gmail.com>
2024-03-02 22:38:30 +0100

    GifsApp project: crear componente lazyimage

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -5,6 +5,7 @@ import { HomePagesComponent } from './pages/home/home-pages.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -16,7 +17,8 @@ import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
     GifsCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePagesComponent
---
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styleUrl: './lazy-image.component.css'
+})
+export class LazyImageComponent {
+
+}
