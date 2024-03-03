1e6c296efc7b59a9bea5999b4c864607d2065cbc
Wilmer Guedez <w.guedex@gmail.com>
2024-03-03 14:16:28 +0100

    CountryApp project: crear componente contact y mostrar en el sidebar

--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
@@ -11,7 +12,8 @@ import { RouterModule } from '@angular/router';
   declarations: [
     HomePageComponent,
     AboutPageComponent,
-    SidebarComponent
+    SidebarComponent,
+    ContactPageComponent
   ],
   imports: [
     CommonModule,
@@ -20,6 +22,7 @@ import { RouterModule } from '@angular/router';
   exports: [
     HomePageComponent,
     AboutPageComponent,
+    ContactPageComponent,
     SidebarComponent
   ]
 })
