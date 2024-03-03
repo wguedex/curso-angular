4d99ad74a1ef906f66f1389f406b11b6c64e8726
Wilmer Guedez <w.guedex@gmail.com>
2024-03-03 20:08:31 +0100

    CountryApp project: Countries Service - Restcountries

---
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl:string = 'https://restcountries.com/v3.1';
+
+  constructor(private httpCliente : HttpClient) { }
+
+  searchCapital(term:string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`
+    return this.httpCliente.get<Country[]>(url);
+
+  }
+
+
+
+
+}
