0a9066fb41be49f98feff1385feb15623df03e08
Wilmer Guedez <w.guedex@gmail.com>
2024-03-07 12:57:25 +0100

    Formularios reactivos: validaciones dinamicas

--- 05-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 05-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  templateUrl: './dynamic-page.component.html'
+  templateUrl: './dynamic-page.component.html',
 })
 export class DynamicPageComponent {
+  public myform: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  get favoriteGames() {
+    return this.myform.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit(): void {
+    if (this.myform.invalid) {
+      this.myform.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myform.value);
+    this.myform.reset();
+  }
 }
