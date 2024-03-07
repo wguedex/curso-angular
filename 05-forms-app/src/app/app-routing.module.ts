018793247ad9e536c894e292fbe5b883e100adad
Wilmer Guedez <w.guedex@gmail.com>
2024-03-07 11:22:51 +0100

    Formularios reactivos: Forms Validator - Validador incluido en Angular

--- 05-forms-app/src/app/app-routing.module.ts
+++ 05-forms-app/src/app/app-routing.module.ts
@@ -4,17 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'reactive',
-    loadChildren: () =>
-      import('./reactive/reactive.module').then((m) => m.ReactiveModule),
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule ),
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
   },
   {
     path: '**',
     redirectTo: 'reactive',
-  },
+  }
 ];
 
 @NgModule({
--- 05-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 05-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  templateUrl: './basic-page.component.html'
+  templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
+  // Forma1:
+  // PRIMER ARREGLO SYNCRONO
+  // SEGUNDO ARREGLO ASYNCRONO
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('', [],[]),
+  //   price: new FormControl(0, [],[]),
+  //   inStorage: new FormControl(0, [],[])
+  // })
+
+  // Forma2:
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3) ], []],
+    price: [0, [Validators.required, Validators.min(0)], []],
+    inStorage: [0, [Validators.min(0)], []],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave() : void{
+
+    if (this.myForm.invalid) return;
+
+    console.log(this.myForm.value)
+  }
 
 }
