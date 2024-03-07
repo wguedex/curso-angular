3628ea7f88bc349ba92e3f1686f9410c78ab06ae
Wilmer Guedez <w.guedex@gmail.com>
2024-03-07 12:28:54 +0100

    Formularios reactivos: metodos de ayuda para los errores

--- 05-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 05-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -25,13 +25,41 @@ export class BasicPageComponent implements OnInit{
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3) ], []],
     price: [0, [Validators.required, Validators.min(0)], []],
-    inStorage: [0, [Validators.min(0)], []],
+    inStorage: [0, [Validators.required, Validators.min(0)], []],
   });
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() : void{
     // this.myForm.reset(RTX5090)
+  }
+
+  isvalidField(field:string): boolean | null{
+    return this.myForm.controls[field].errors &&
+           this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field:string): string | null{
+
+    // if (!this.myForm.controls[field] &&  !this.myForm.controls[field].errors)
+    if (!this.myForm.controls[field] )
+    { return null };
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      // console.log(key)
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return 'Este requiere minimo 3 letras'
+
+      }
+    }
+
+    return null;
+
   }
 
   onSave() : void{
