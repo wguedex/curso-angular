3da479794fb48a0698313be17969b7ea18d0a3c8
Wilmer Guedez <w.guedex@gmail.com>
2024-03-07 13:29:32 +0100

    Formularios reactivos: eliminar elementos de un FormArray

--- 05-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 05-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -20,6 +20,40 @@ export class DynamicPageComponent {
     return this.myform.get('favoriteGames') as FormArray;
   }
 
+
+  isvalidField(field:string): boolean | null{
+    return this.myform.controls[field].errors &&
+           this.myform.controls[field].touched;
+  }
+
+  getFieldError(field:string): string | null{
+
+    // if (!this.myForm.controls[field] &&  !this.myForm.controls[field].errors)
+    if (!this.myform.controls[field] )
+    { return null };
+
+    const errors = this.myform.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      // console.log(key)
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return 'Este requiere minimo 3 letras'
+
+      }
+    }
+
+    return null;
+
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].errors &&
+          formArray.controls[index].touched;
+  }
+
   onSubmit(): void {
     if (this.myform.invalid) {
       this.myform.markAllAsTouched();
