8f5e30e3264a1e04413013d9d90bfb72aaffb2e2
Wilmer Guedez <w.guedex@gmail.com>
2024-03-08 00:03:42 +0100

    Formularios reactivos: formulario de registro

--- 05-forms-app/src/app/auth/auth.module.ts
+++ 05-forms-app/src/app/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- 05-forms-app/src/app/auth/pages/register-page/register-page.component.ts
+++ 05-forms-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html'
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor( private fb: FormBuilder ) {}
+
+  isValidField( field: string ) {
+    // TODO: obtener validación desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
