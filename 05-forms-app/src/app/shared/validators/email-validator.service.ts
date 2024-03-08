dddee85d318a6242a1b36f1557bfc9bb8b55c129
Wilmer Guedez <w.guedex@gmail.com>
2024-03-08 09:19:31 +0100

    Formularios reactivos: elaborar el validador de correo

--- 05-forms-app/src/app/shared/validators/email-validator.service.ts
+++ 05-forms-app/src/app/shared/validators/email-validator.service.ts
@@ -11,12 +11,43 @@ export class EmailValidatorService implements OnInit, AsyncValidator {
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
 
     const email = control.value;
+    console.log({email})
 
     //crear observable
-   return  of({
-      emailTaken: true,
-    }).pipe(delay(2000));
+  //  return  of({
+  //     emailTaken: {
+  //       message:'email is taken'
+  //     },
+  //   }).pipe(delay(2000));
+
+  const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) =>{
+    console.log({email});
+
+    if (email==='fernando@gmail.com'){
+      subscriber.next({emailTaken:true})
+      subscriber.complete();
+      return;
+    }
+
+    subscriber.next(null);
+    subscriber.complete();
+
+  } ).pipe(delay(2000));
+
+  return httpCallObservable;
+
   }
 
   ngOnInit(): void {}
 }
+
+
+// return this.http.get<any[]>(`http://localhost:3000/users?q=${ email }`)
+// .pipe(
+//     // delay(2000),
+//     map(resp => {
+//         return (resp.length === 0)
+//             ? null
+//             : { emailTaken: true }
+//     })
+// );
