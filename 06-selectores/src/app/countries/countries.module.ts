413387cf204e8b7779a295de790cbb1ffb0c926c
Wilmer Guedez <w.guedex@gmail.com>
2024-03-08 11:03:42 +0100

    Formularios reactivos multiple: Estructuras de directorios para esta aplicación

---
+++ 06-selectores/src/app/countries/countries.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CountriesRoutingModule } from './countries-routing.module';
+
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    SelectorPageComponent
+  ],
+  imports: [
+    CommonModule,
+    CountriesRoutingModule
+  ],
+  providers: [ /* servicios */]
+})
+export class CountriesModule { }
---
+++ 06-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-selector-page',
+  templateUrl: './selector-page.component.html'
+})
+export class SelectorPageComponent {
+
+}
