d4ac1c616c3a4e0c05ad54eef81adbd03c63ed8d
Wilmer Guedez <w.guedex@gmail.com>
2024-03-11 11:53:16 +0100

    CountryApp project: Buscar por región - Optimización

--- 04-countryApp/src/app/countries/pages/by-region-pages/by-region-pages.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-pages/by-region-pages.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'app-by-region-pages',
   templateUrl: './by-region-pages.component.html',
@@ -14,19 +16,21 @@ import { Country } from '../../interfaces/country';
 export class ByRegionPagesComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa','Americas','Asia','Europe','Oceania'];
+  public selectedRegion? : Region;
 
   constructor(private countriesService : CountriesService) {
 
   }
 
-  onSearchByRegion(value:string): void{
+  onSearchByRegion(region:Region): void{
 
-    this.countriesService.searchRegion(value).subscribe(countries =>{
+    this.selectedRegion = region;
+
+    this.countriesService.searchRegion(region).subscribe(countries =>{
       this.countries = countries;
     })
 
   }
-
-
 
 }
