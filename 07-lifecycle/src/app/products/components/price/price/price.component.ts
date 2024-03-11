182f9bbc160fe2fc781ecd24a580d652f528caae
Wilmer Guedez <w.guedex@gmail.com>
2024-03-11 09:16:03 +0100

    LifeCycle hooks: implementar ngOnDestroy

--- 07-lifecycle/src/app/products/components/price/price/price.component.ts
+++ 07-lifecycle/src/app/products/components/price/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -9,12 +10,15 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy{
 
   @Input() price : number = 0;
 
+  public interval$?: Subscription;
+
   constructor(){
     console.log('Componente HIJO')
   }
 
   ngOnInit(): void {
-    console.log()
+    console.log('ngOnInit')
+    this.interval$ = interval(1000).subscribe( value => console.log(`Tick: ${value}`) );
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -24,6 +28,7 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy{
 
   ngOnDestroy(): void {
     console.log('Componente HIJO: ngOnDestroy')
+    this.interval$?.unsubscribe();
   }
 
 }
