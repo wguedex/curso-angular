b6f6d22e02eaee10aa360bf009b9e5fadad9abf1
Wilmer Guedez <w.guedex@gmail.com>
2024-03-17 15:01:49 +0100

    PipesApp - PrimeNG: Módulos y rutas de mi aplicación

---
+++ 08-pipesApp/src/app/products/products.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsComponent } from './products.component';
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+
+
+@NgModule({
+  declarations: [
+    ProductsComponent,
+    BasicsPageComponent,
+    NumbersPageComponent,
+    UncommonPageComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule
+  ]
+})
+export class ProductsModule { }
