379e8846386c071051233ffdf73c113d29ca896e
Wilmer Guedez <w.guedex@gmail.com>
2024-03-20 10:12:48 +0100

    Pipes Personalizados: PrimeNG Table

--- 08-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 08-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 
-// import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
-import { ToolbarModule } from 'primeng/toolbar';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+
 
 @NgModule({
   declarations: [],
@@ -20,7 +21,8 @@ import { SplitButtonModule } from 'primeng/splitbutton';
     MenubarModule,
     PanelModule,
     ToolbarModule,
-    SplitButtonModule
+    SplitButtonModule,
+    TableModule
   ]
 })
 export class PrimeNGModule { }
--- 08-pipesApp/src/app/products/pages/order/order.component.ts
+++ 08-pipesApp/src/app/products/pages/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'products-order',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 export class OrderComponent {
 
   public isUpperCase: boolean = false;
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue
+    },
+    {
+      name: 'batman',
+      canFly: false,
+      color: Color.black
+    },
+    {
+      name: 'daredevil',
+      canFly: false,
+      color: Color.red
+    },
+    {
+      name: 'Robin',
+      canFly: false,
+      color: Color.green
+    },
+    {
+      name: 'Linterna Verde',
+      canFly: true,
+      color: Color.green
+    }
+  ]
 
   toggleUpperCase():void {
     this.isUpperCase = !this.isUpperCase;
