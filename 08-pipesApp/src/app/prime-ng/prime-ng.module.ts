d1c3ccbe2977136e630fdbe037cedf3fffea1dc0
Wilmer Guedez <w.guedex@gmail.com>
2024-03-20 09:52:55 +0100

    Pipes Personalizados: valores y argumentos personalizados al pipe

--- 08-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 08-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -6,6 +6,8 @@ import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @NgModule({
   declarations: [],
@@ -16,7 +18,9 @@ import { PanelModule } from 'primeng/panel';
     CardModule,
     FieldsetModule,
     MenubarModule,
-    PanelModule
+    PanelModule,
+    ToolbarModule,
+    SplitButtonModule
   ]
 })
 export class PrimeNGModule { }
--- 08-pipesApp/src/app/products/pipes/toggle-case.pipe.ts
+++ 08-pipesApp/src/app/products/pipes/toggle-case.pipe.ts
@@ -6,8 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class ToggleCasePipe implements PipeTransform {
 
-  transform(value: string ) : string {
-    return value.toUpperCase();
+  transform(value: string, toUpper: boolean ) : string {
+
+    console.log({value, toUpper});
+
+    return ( toUpper)
+    ? value.toUpperCase()
+    : value.toLowerCase();
   }
 
 }
