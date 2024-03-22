082c6f6ec32985a85ce4e67e2ad57d89baa30653
Wilmer Guedez <w.guedex@gmail.com>
2024-03-22 12:55:04 +0100

    Angular material: Diseño de login y Auth Layout

--- 09-heroesApp/src/app/auth/auth.module.ts
+++ 09-heroesApp/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -15,7 +16,8 @@ import { AuthRoutingModule } from './auth-routing.module';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
