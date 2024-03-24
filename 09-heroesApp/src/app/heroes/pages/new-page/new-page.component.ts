cd07ab532f12535f583415425633cdab2f1a90a0
Wilmer Guedez <w.guedex@gmail.com>
2024-03-24 10:47:09 +0100

    Angular material: HeroForm - ReactiveForms introducción

--- 09-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 09-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
-  templateUrl: './new-page.component.html'
+  templateUrl: './new-page.component.html',
 })
 export class NewPageComponent {
+  public heroForm = new FormGroup({
+    id:               new FormControl<string>(''),
+    superhero:        new FormControl<string>('',{nonNullable:true}),
+    publisher:        new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego:        new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters:       new FormControl(''),
+    alt_img:          new FormControl(''),
+  });
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
