a69318ca9735cb5029f634e7a009c37ae6184e8b
Wilmer Guedez <w.guedex@gmail.com>
2024-03-26 23:30:32 +0100

    Angular material: mantener la sesion del usuario

--- 09-heroesApp/src/app/app.component.ts
+++ 09-heroesApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './heroes/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '09-heroesApp';
+
+  constructor( ){}
+
+
+
 }
--- 09-heroesApp/src/app/heroes/services/auth.service.ts
+++ 09-heroesApp/src/app/heroes/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, of, map, catchError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -28,6 +28,21 @@ export class AuthService {
     tap( user => localStorage.setItem('token',user.id.toString())),
 
    );
+  }
+
+  checkAuthentication(): Observable<boolean>{
+
+    if (!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+    .pipe(
+      tap(user => this.user = user),
+      map(user => !!user),
+      catchError(err=> of(false))
+    );
+
   }
 
   logout(){
