d39fe75f66dac15703308fb6411cf73270f0bf5d
Wilmer Guedez <w.guedex@gmail.com>
2024-03-29 12:45:45 +0100

    Mapas en angular: Obtener las coordenadas centrales del mapa

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,13 +1,23 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 
-import { Map } from 'mapbox-gl';
+import { Map, LngLat } from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
-  styleUrl: './zoom-range-page.component.css'
+  styleUrl: './zoom-range-page.component.css',
 })
-export class ZoomRangePageComponent implements AfterViewInit{
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
 
   @ViewChild('map')
   public divMap?: ElementRef;
@@ -15,49 +25,52 @@ export class ZoomRangePageComponent implements AfterViewInit{
   public zoom: number = 10;
   public map?: Map;
 
-  ngAfterViewInit(): void {
+  public currentLngLat: LngLat = new LngLat(-74.5, 40);
 
+  ngAfterViewInit(): void {
     if (!this.divMap) throw 'El elemento HTML no fue encontrado';
 
-    console.log(this.divMap)
+    console.log(this.divMap);
 
     this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListeners();
-
   }
 
-  mapListeners(){
-
-    if (!this.map){
-      throw 'Mapa no inicializado'
+  mapListeners() {
+    if (!this.map) {
+      throw 'Mapa no inicializado';
     }
 
-    this.map?.on('zoom',(event)=>{
-       this.zoom = this.map!.getZoom();
-    })
+    this.map?.on('zoom', (event) => {
+      this.zoom = this.map!.getZoom();
+    });
 
-    this.map?.on('zoomend',(event)=>{
-      if ( this.map!.getZoom() < 18 ) return;
+    this.map?.on('zoomend', (event) => {
+      if (this.map!.getZoom() < 18) return;
       this.map!.zoomTo(18);
-   })
+    });
 
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+      console.log(this.currentLngLat);
+    });
   }
 
-  zoomIn(){
+  zoomIn() {
     this.map?.zoomIn();
   }
 
-  zoomOut(){
+  zoomOut() {
     this.map?.zoomOut();
   }
 
-  zoomChanged(value : string){
+  zoomChanged(value: string) {
     this.zoom = Number(value);
     this.map?.zoomTo(this.zoom);
   }
