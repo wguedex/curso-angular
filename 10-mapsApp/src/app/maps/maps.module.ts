444bddf30bbc63f8aa5110b8ddc85b8456239b9e
Wilmer Guedez <w.guedex@gmail.com>
2024-03-30 00:35:01 +0100

    Mapas en angular: componente miniMapa

--- 10-mapsApp/src/app/maps/maps.module.ts
+++ 10-mapsApp/src/app/maps/maps.module.ts
@@ -32,35 +32,3 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
 export class MapsModule { }
 
 
-// import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-
-// import { MapsRoutingModule } from './maps-routing.module';
-// import { MiniMapComponent } from './components/mini-map/mini-map.component';
-// import { SideMenuComponent } from './components/side-menu/side-menu.component';
-// import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
-// import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
-// import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
-// import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
-// import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
-
-// import mapboxgl from 'mapbox-gl';
-// (mapboxgl as any).accessToken = 'pk.eyJ1Ijoid2d1ZWRleCIsImEiOiJjbHViYTVoNHMwc2tpMmpxbjBpbzN5eThmIn0.U-ekrWuFrtSOwPB5f2Z-8A';
-
-
-// @NgModule({
-//   declarations: [
-//     MiniMapComponent,
-//     SideMenuComponent,
-//     MapsLayoutComponent,
-//     FullScreenPageComponent,
-//     MarkersPageComponent,
-//     PropertiesPageComponent,
-//     ZoomRangePageComponent
-//   ],
-//   imports: [
-//     CommonModule,
-//     MapsRoutingModule
-//   ]
-// })
-// export class MapsModule { }
