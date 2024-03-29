149da7179bbdb2fa8a543238891166aa1a5e16ea
Wilmer Guedez <w.guedex@gmail.com>
2024-03-30 00:19:25 +0100

    Mapas en angular: componente miniMapa

--- 10-mapsApp/src/app/maps/layout/maps-layout/maps-layout.component.ts
+++ 10-mapsApp/src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   templateUrl: './maps-layout.component.html',
   styleUrls: ['./maps-layout.component.css']
 })
 export class MapsLayoutComponent {
+
+  @Input()
+  lngLat?: [number,number];
+
+  ngAfterViewInit(){
+
+    if (!this.lngLat) throw "LngLat can't be null";
+
+    // mapa
+
+    // marker
+
+
+
+  }
 
 }
