b4c82c666c94d809f4e27d1b0159debb1aa4abdb
Wilmer Guedez <w.guedex@gmail.com>
2024-03-30 09:50:57 +0100

    Mapas en angular: Standalone - como pagina

---
+++ 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alone-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './alone-page.component.html',
+  styleUrl: './alone-page.component.css'
+})
+export class AlonePageComponent {
+
+}
