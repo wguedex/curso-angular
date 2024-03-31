409b3ad4ab993ce09a0abe36bf58b1d71dbf5fcd
Wilmer Guedez <w.guedex@gmail.com>
2024-03-31 23:44:12 +0200

    Directivas personalizadas: estructura de la aplicación

--- 11-directiveSignals/src/app/app-routing.module.ts
+++ 11-directiveSignals/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './products/pages/product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'products',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
