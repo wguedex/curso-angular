22e0bbb2639a3757da4b6d1a39a8f11c9d17dd76
Wilmer Guedez <w.guedex@gmail.com>
2024-04-01 23:40:39 +0200

    Directivas personalizadas: formulario reactivo tradicional

--- 11-directiveSignals/src/app/products/pages/product-page/product-page.component.ts
+++ 11-directiveSignals/src/app/products/pages/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-page',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './product-page.component.css'
 })
 export class ProductPageComponent {
+
+
+
+  //Otra forma de crear el objeto formbuilder
+  private fb = inject(FormBuilder)
+
+  public color:string = 'green';
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required, Validators.minLength(6), Validators.email]
+  });
+
 
 }
--- 11-directiveSignals/src/app/products/products.module.ts
+++ 11-directiveSignals/src/app/products/products.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
@@ -7,12 +8,13 @@ import { ProductPageComponent } from './pages/product-page/product-page.componen
 
 @NgModule({
   declarations: [
-  
+
     ProductPageComponent
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
