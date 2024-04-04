5bdbfe3bbdd4091419fd7cb4c6c7e5ffb3470d79
Wilmer Guedez <w.guedex@gmail.com>
2024-04-04 21:44:09 +0200

    Signals en angular: Primera señal signal

--- 11-directiveSignals/src/app/signals/components/side-menu/side-menu.component.ts
+++ 11-directiveSignals/src/app/signals/components/side-menu/side-menu.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface MenuItem {
-  title:string;
-  route:string;
+  title: string;
+  route: string;
 }
 
 @Component({
   selector: 'side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.css'
+  styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
-  public menuItem: MenuItem[] = [
-    { title:'Contador', route:'counter' },
-    { title:'Usuarios', route:'user-info' },
-    { title:'Mutaciones', route:'properties' },
-  ]
+
+  public menuItems = signal<MenuItem[]>([
+    { title: 'Contador', route: 'counter' },
+    { title: 'Usuarios', route: 'user-info' },
+    { title: 'Mutaciones', route: 'properties' },
+  ]);
+
+  // public menuItem: MenuItem[] = [
+  //   { title: 'Contador', route: 'counter' },
+  //   { title: 'Usuarios', route: 'user-info' },
+  //   { title: 'Mutaciones', route: 'properties' },
+  // ];
 }
