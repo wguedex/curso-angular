10fc144307bb14810983614b04d4626cf7c1cb5d
Wilmer Guedez <w.guedex@gmail.com>
2024-04-05 12:10:40 +0200

    Signals en angular: Counter signal

--- 11-directiveSignals/src/app/signals/pages/counter-page/counter-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './counter-page.component.css'
 })
 export class CounterPageComponent {
+
+  public counter = signal(10);
+
+  public squareCounter = computed( () => this.counter() * this.counter() );
+
+  increase(value : number){
+    this.counter.update(current => current + value);
+  }
+
+  dicrease(value : number){
+    this.counter.update(current => current - value);
+  }
 
 }
