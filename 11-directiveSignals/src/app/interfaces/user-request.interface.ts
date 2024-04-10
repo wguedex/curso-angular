ce13c18dcd35e9860ce53152a99643d1a6af43f8
Wilmer Guedez <w.guedex@gmail.com>
2024-04-10 09:29:05 +0200

    Signals en angular: Informacion de usuario

---
+++ 11-directiveSignals/src/app/interfaces/user-request.interface.ts
@@ -0,0 +1,11 @@
+export interface SingleUserResponse {
+  data:    User;
+}
+
+export interface User {
+  id:         number;
+  email:      string;
+  first_name: string;
+  last_name:  string;
+  avatar:     string;
+}
