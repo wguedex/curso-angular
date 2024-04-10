ec14b963a086881537c326e670b710a7e9e1b72a
Wilmer Guedez <w.guedex@gmail.com>
2024-04-10 09:54:55 +0200

    Signals en angular: obtener informacion de usuario desde el backend

--- 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersService } from '../../../services/user-service.service';
+import { User } from '../../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit{
+
+  private usersService = inject(UsersService);
+  public userId = signal(1);
+
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId())
+  }
+
+  loadUser(id:number){
+    if ( id <= 0 ) return;
+
+    this.userId.set(id);
+
+    this.usersService.getUserById(id)
+    .subscribe(user=>{
+      console.log({user})
+    })
+
+  }
 
 }
