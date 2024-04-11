c8cc584bccf9b779eb4f24e7c95d0d9af56dd361
Wilmer Guedez <w.guedex@gmail.com>
2024-04-11 17:24:56 +0200

    Signals en angular: Manejo de errores y propiedades computadas

--- 11-directiveSignals/src/app/signals/layout/signals-layout/signals-layout.component.ts
+++ 11-directiveSignals/src/app/signals/layout/signals-layout/signals-layout.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-signals-layout',
   templateUrl: './signals-layout.component.html',
-  styleUrl: './signals-layout.component.css'
+  styleUrls: ['./signals-layout.component.css']
 })
 export class SignalsLayoutComponent {
 
 }
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-signals-layout',
+//   templateUrl: './signals-layout.component.html',
+//   styleUrl: './signals-layout.component.css'
+// })
+// export class SignalsLayoutComponent {
+
+// }
--- 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,34 +1,92 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
-import { UsersService } from '../../../services/user-service.service';
-import { User } from '../../../interfaces/user-request.interface';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+
+import { UsersService } from '../../services/user-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
-  selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
-  styleUrl: './user-info-page.component.css'
+  styleUrls: ['./user-info-page.component.css']
 })
-export class UserInfoPageComponent implements OnInit{
+export class UserInfoPageComponent implements OnInit {
 
   private usersService = inject(UsersService);
   public userId = signal(1);
 
   public currentUser = signal<User|undefined>(undefined);
   public userWasFound = signal(true);
+  public fullName = computed<string>( () => {
+
+    if (!this.currentUser()) {
+      return 'Usuario no encontrado';
+    }
+    return `${ this.currentUser()!.first_name } ${ this.currentUser()!.last_name }`;
+
+  });
+
+  // public fullName = computed<string>( () => {
+  //   if ( !this.currentUser() ) return 'Usuario no encontrado';
+  //   return `${ this.currentUser()?.first_name } ${ this.currentUser()?.last_name }`;
+  // });
+
 
   ngOnInit(): void {
-    this.loadUser(this.userId())
+    this.loadUser( this.userId() )
   }
 
-  loadUser(id:number){
+  loadUser( id: number ) {
     if ( id <= 0 ) return;
 
     this.userId.set(id);
+    this.currentUser.set(undefined);
 
-    this.usersService.getUserById(id)
-    .subscribe(user=>{
-      console.log({user})
-    })
+    this.usersService.getUserById( id )
+      .subscribe({
+        next: (user) => {
+         this.currentUser.set( user );
+         this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        },
+      });
+
 
   }
 
 }
+
+// import { Component, OnInit, inject, signal } from '@angular/core';
+// import { UsersService } from '../../../services/user-service.service';
+// import { User } from '../../../interfaces/user-request.interface';
+
+// @Component({
+//   selector: 'app-user-info-page',
+//   templateUrl: './user-info-page.component.html',
+//   styleUrl: './user-info-page.component.css'
+// })
+// export class UserInfoPageComponent implements OnInit{
+
+//   private usersService = inject(UsersService);
+//   public userId = signal(1);
+
+//   public currentUser = signal<User|undefined>(undefined);
+//   public userWasFound = signal(true);
+
+//   ngOnInit(): void {
+//     this.loadUser(this.userId())
+//   }
+
+//   loadUser(id:number){
+//     if ( id <= 0 ) return;
+
+//     this.userId.set(id);
+
+//     this.usersService.getUserById(id)
+//     .subscribe(user=>{
+//       console.log({user})
+//     })
+
+//   }
+
+// }
