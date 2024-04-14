4f07a3fddb092ad16d9bb6a600df8ffa7756af6b
Wilmer Guedez <w.guedex@gmail.com>
2024-04-14 17:06:35 +0200

    Signals en angular: Mutaciones - Actualizar parte del estado

--- 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.css'
+  styleUrl: './properties-page.component.css',
 })
 export class PropertiesPageComponent {
+  public user = signal<User>({
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
 
-  onFieldUpdated(field:string, value : string){
-    console.log({field, value})
+  public fullName = computed(
+    () => `${this.user().first_name} ${this.user().last_name}`
+  );
+
+  onFieldUpdated(field: keyof User, value: string) {
+    // console.log({field, value})
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value
+    // })
+
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+
+      return {
+        ...current,
+      };
+    });
   }
-
 }
