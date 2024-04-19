b4d070d45cafa0f8849a7beb749d9cc10efb0ba3
Wilmer Guedez <w.guedex@gmail.com>
2024-04-18 21:13:21 -0500

    12-nestbackend: Conectar Nest con MongoDB

--- 12-nestBackend/src/app.module.ts
+++ 12-nestBackend/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module'; 
+ 
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({
-  imports: [AuthModule],
+@Module({ 
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
