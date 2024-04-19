bb9087d53f3cdea50520f8479e9a18ff327a4940
Wilmer Guedez <w.guedex@gmail.com>
2024-04-19 04:58:55 -0500

    12-nestbackend: Crear modelo y esquema

--- 12-nestBackend/src/auth/auth.module.ts
+++ 12-nestBackend/src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, userSchema } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: userSchema }])],
 })
-export class AuthModule {}
+export class AuthModule {
+
+}
---
+++ 12-nestBackend/src/auth/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User {
+  @Prop({
+    unique: true,
+    required: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    minlength: 6,
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    type: [String],
+    default:['user']
+  })
+  roles: string[];
+
+
+  @Prop({
+    default: true,
+  })
+  isActive: string;
+}
+
+export const userSchema = SchemaFactory.createForClass(User);
