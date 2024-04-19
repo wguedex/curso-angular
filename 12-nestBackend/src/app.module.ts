cdd9f13fd30e73eaa76873bf02ce48c6d1e003dc
Wilmer Guedez <w.guedex@gmail.com>
2024-04-19 04:25:57 -0500

    12-nestbackend: agregar variables de entorno

--- 12-nestBackend/src/app.module.ts
+++ 12-nestBackend/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module'; 
- 
-import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
-@Module({ 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
-    AuthModule
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
