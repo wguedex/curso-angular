cb6c15881aafc3b3b872ee411fce77230f20f318
Wilmer Guedez <w.guedex@gmail.com>
2024-04-22 16:54:45 -0500

    12-nestbackend: Generar un nuevo JWT

--- 12-nestBackend/src/auth/auth.controller.ts
+++ 12-nestBackend/src/auth/auth.controller.ts
@@ -1,8 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
- 
+
 import { CreateUserDto, RegisterUserDto, UpdateAuthDto, LoginDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from '../../dist/auth/interfaces/login-response';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -13,20 +25,20 @@ export class AuthController {
     return this.authService.create(CreateUserDto);
   }
 
-  @Post('/login') 
-  login( @Body() loginDto: LoginDto ){
+  @Post('/login')
+  login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
- 
-  @Post('/register') 
-  register(@Body() registerUserDto: RegisterUserDto ){
+
+  @Post('/register')
+  register(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
   }
 
-  @UseGuards( AuthGuard )
+  @UseGuards(AuthGuard)
   @Get()
-  findAll( @Request() req: Request ) {
-    console.log(req)
+  findAll(@Request() req: Request) {
+    console.log(req);
     const user = req['user'];
     return user;
     // return this.authService.findAll();
@@ -46,4 +58,21 @@ export class AuthController {
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
   }
+ 
+  // LoginResponse
+  // @UseGuards( AuthGuard )
+  // @Get('check-token')
+  // checkToken( @Request() req: Request ): LoginResponse {
+      
+  //   const user = req['user'] as User;
+
+  //   console.log(user)
+
+  //   return {
+  //     user,
+  //     token: this.authService.getJwtToken({ id: user._id })
+  //   }
+
+  // }
+
 }
