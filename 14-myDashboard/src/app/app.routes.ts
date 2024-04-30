b0e4c1b1edb756553cadccf2de5d028c0e8b8ea7
Wilmer Guedez <w.guedex@gmail.com>
2024-04-29 23:37:01 -0500

    Angular 17+ - Nuevas Caracteristicas: Configuración de rutas

--- 14-myDashboard/src/app/app.routes.ts
+++ 14-myDashboard/src/app/app.routes.ts
@@ -1,3 +1,53 @@
 import { Routes } from '@angular/router';
+import { ChangeDetectionComponent } from './dashboard/pages/change-detection/change-detection.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then( c => c.DashboardComponent),
+    children: [
+      {
+        path : 'change-detection',
+        title: 'Change Detection',
+        loadComponent: () => import('./dashboard/pages/change-detection/change-detection.component').then( c => c.ChangeDetectionComponent),
+      },
+      {
+        path : 'control-flow',
+        title: 'Control Flow',
+        loadComponent: () => import('./dashboard/pages/control-flow/control-flow.component').then( c => c.ControlFlowComponent),
+      },
+      {
+        path : 'defer-options',
+        title: 'Defer Options',
+        loadComponent: () => import('./dashboard/pages/defer-options/defer-options.component').then( c => c.DeferOptionsComponent),
+      },
+      {
+        path : 'defer-views',
+        title: 'Defer Views',
+        loadComponent: () => import('./dashboard/pages/defer-views/defer-views.component').then( c => c.DeferViewsComponent),
+      },
+      {
+        path : 'users',
+        title: 'Users',
+        loadComponent: () => import('./dashboard/pages/users/users.component').then( c => c.UsersComponent),
+      },
+      {
+        path : 'user',
+        title: 'User',
+        loadComponent: () => import('./dashboard/pages/user/user.component').then( c => c.UserComponent),
+      },
+      {
+        path : 'view-transition',
+        title: 'View Transition',
+        loadComponent: () => import('./dashboard/pages/view-transition/view-transition.component').then( c => c.ViewTransitionComponent),
+      },
+      {
+        path:'', redirectTo: 'control-flow', pathMatch: 'full',
+      }
+    ]
+  }, {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+];
