396b3ad626965b635589c77eb04b88ef32312cb2
Wilmer Guedez <w.guedex@gmail.com>
2024-05-01 17:27:20 -0500

    Angular 17+ - Nuevas Caracteristicas: Control Flow - @For

--- 14-myDashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ 14-myDashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -18,6 +18,9 @@ export class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>('A');
 
+  public frameWorks  = signal(['Angular','Vue','Svelte','Qwik','React']);
+  public frameWorks2 = signal([]);
+
   public toggleContent() {
     this.showContent.update(value => !value)
   }
