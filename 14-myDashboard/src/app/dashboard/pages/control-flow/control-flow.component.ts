2123af2702cd2eea3574a46f9725cc599a63b1a2
Wilmer Guedez <w.guedex@gmail.com>
2024-05-01 16:50:02 -0500

    Angular 17+ - Nuevas Caracteristicas: control flow @switch

--- 14-myDashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ 14-myDashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'C' | 'D' | 'E' | 'F'
+
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -14,10 +16,10 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 export class ControlFlowComponent {
 
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
 
   public toggleContent() {
     this.showContent.update(value => !value)
-
   }
 
 }
