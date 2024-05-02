75ebe734eb306b5c0ba68d720912bdd120648589
Wilmer Guedez <w.guedex@gmail.com>
2024-05-02 07:36:08 -0500

    Angular 17+ - Nuevas Caracteristicas: defer tiggers

--- 14-myDashboard/src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ 14-myDashboard/src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
   imports: [
-    CommonModule, HeavyLoadersSlowComponent
+    CommonModule, HeavyLoadersSlowComponent, TitleComponent
   ],
   templateUrl: './defer-views.component.html',
   styleUrl: './defer-views.component.css',
