d730364e4920c7e55d2ab0932f7eab9efc1fc9f1
Wilmer Guedez <w.guedex@gmail.com>
2024-05-02 07:27:26 -0500

    Angular 17+ - Nuevas Caracteristicas: defer blocks

--- 14-myDashboard/src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ 14-myDashboard/src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, HeavyLoadersSlowComponent
   ],
-  template: `<p>defer-views works!</p>`,
+  templateUrl: './defer-views.component.html',
   styleUrl: './defer-views.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DeferViewsComponent { }
+export class DeferViewsComponent {
+
+
+}
--- 14-myDashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ 14-myDashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -15,15 +15,13 @@ export class HeavyLoadersSlowComponent {
 
   @Input({ required: true }) cssClass!: string;
 
-
-
   constructor() {
+
 
     const start = Date.now();
     while( Date.now() - start < 3000 ) {}
 
     console.log('Cargado');
   }
-
 
 }
