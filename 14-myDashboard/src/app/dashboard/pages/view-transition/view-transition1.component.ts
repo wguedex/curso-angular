a5f3109666e11df422ff9851c95aad6c9f02a213
Wilmer Guedez <w.guedex@gmail.com>
2024-05-02 10:29:13 -0500

    Angular 17+ - Nuevas Caracteristicas: Hero animation - view Transitions

--- 14-myDashboard/src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ 14-myDashboard/src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -14,7 +14,7 @@ import { TitleComponent } from '@shared/title/title.component';
 
   <section class="flex justify-start">
 
-  <img srcset="https://picsum.photos/id/237/200/300" alt="picsum" width="200" height="300">
+  <img srcset="https://picsum.photos/id/237/200/300" alt="picsum" width="200" height="300" style="view-transition-name: hero1">
 
   <div class="bg-blue-500 w-56 h-56"></div>
 
--- 14-myDashboard/src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ 14-myDashboard/src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -10,13 +10,13 @@ import { TitleComponent } from '@shared/title/title.component';
   ],
   template: `
 
-  <app-title title="View Transition"/>
+  <app-title title="View Transition 2"/>
 
-  <section class="flex justify-start">
+  <section class="flex justify-end">
 
-  <img srcset="https://picsum.photos/id/237/200/300" alt="picsum" width="200" height="300">
+  <img srcset="https://picsum.photos/id/237/200/300" alt="picsum" width="200" height="300" style="view-transition-name: hero2">
 
-  <div class="bg-blue-500 w-56 h-56"></div>
+  <div class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded"></div>
 
   </section>
 
